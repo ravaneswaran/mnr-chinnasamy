7c7f8107ebaa3610ee20c0c0ef75aaec785161be
ravaneswaran <ravaneswaran@gmail.com>
2021-05-26 16:58:42 +0530

    first commit on the main branch

---
+++ src/main/java/com/store/TheStoreApplication.java
@@ -0,0 +1,13 @@
+package com.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheStoreApplication.class, args);
+	}
+
+}
