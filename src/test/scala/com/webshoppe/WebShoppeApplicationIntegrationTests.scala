c1b6feb3a48cc4c69132ef8b5c6402868998e9af
ravaneswaran <ravaneswaran@gmail.com>
2021-06-04 20:59:32 +0530

    integration test folder 'scala' is created

---
+++ src/test/scala/com/webshoppe/WebShoppeApplicationIntegrationTests.scala
@@ -0,0 +1,14 @@
+package com.webshoppe
+
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebShoppeApplicationIntegrationTests {
+
+  @Test
+  def contextLoads(): Unit = {
+  }
+
+}
