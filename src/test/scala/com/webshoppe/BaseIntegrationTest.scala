a605f37aa214c1928b33bfd5d31fe2daef71fcf0
ravaneswaran <ravaneswaran@gmail.com>
2021-06-08 23:44:35 +0530

    Resource Not Found integration test included

---
+++ src/test/scala/com/webshoppe/BaseIntegrationTest.scala
@@ -0,0 +1,31 @@
+package com.webshoppe
+
+import io.cucumber.scala.{EN, ScalaDsl}
+import org.junit.jupiter.api.{AfterAll, BeforeAll}
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.firefox.FirefoxDriver
+
+class BaseIntegrationTest extends ScalaDsl with EN{
+
+  var webDriver: WebDriver = null
+
+  @BeforeAll
+  def setUP():Unit = {
+    System.out.println("Before All")
+    System.setProperty("webdriver.gecko.driver","classpath:geckodriver");
+    this.webDriver = new FirefoxDriver()
+  }
+
+  def getWebDriver(): WebDriver = {
+    this.webDriver;
+  }
+
+  @AfterAll
+  def closeWebDriver(): Unit = {
+    System.out.println("After All")
+    this.webDriver.close()
+    this.webDriver = null
+  }
+
+}
+
--- src/test/scala/com/webshoppe/ResourceNotFoundIntegrationTest.scala
+++ src/test/scala/com/webshoppe/ResourceNotFoundIntegrationTest.scala
@@ -2,30 +2,41 @@ package com.webshoppe
 
 import io.cucumber.junit.{Cucumber, CucumberOptions}
 import io.cucumber.scala.{EN, ScalaDsl}
+import io.cucumber.spring.CucumberContextConfiguration
 import org.junit.runner.RunWith
-
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.firefox.FirefoxDriver
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment
+import org.springframework.http.HttpStatus
 
 @RunWith(classOf[Cucumber])
 @CucumberOptions(
   features = Array("classpath:features/resource-not-found.feature"),
-  glue = Array("com.webshoppe")/*,
-  plugin = Array("pretty", "html:target/cucumber/html")*/)
-class ResourceNotFoundIntegrationTest extends ScalaDsl with EN{
+  glue = Array("com.webshoppe"))
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
+@CucumberContextConfiguration
+class ResourceNotFoundIntegrationTest extends ScalaDsl with EN {
+
+  var webDriver: WebDriver = null
 
   Given("""A user opens up a browser`""") { () =>
-    // Write code here that turns the phrase above into concrete actions
-    //throw new io.cucumber.scala.PendingException()
+    System.setProperty("webdriver.gecko.driver","src/test/resources/geckodriver")
+    this.webDriver = new FirefoxDriver()
   }
-  When("""He hits the following url {string}""") { (string: String) =>
-    // Write code here that turns the phrase above into concrete actions
-    //throw new io.cucumber.scala.PendingException()
+
+  When("""He hits the following url {string}""") { (url: String) =>
+    this.webDriver.get(url)
   }
-  Then("""The browser should open a {int} page""") { (int1: Int) =>
-    // Write code here that turns the phrase above into concrete actions
-    //throw new io.cucumber.scala.PendingException()
+
+  Then("""The browser should open a {int} page""") { (httpStatus: Int) =>
+    assert(httpStatus == HttpStatus.NOT_FOUND.value())
   }
-  Then("""{string} should be the title of the page.""") { (string: String) =>
-    // Write code here that turns the phrase above into concrete actions
-    //throw new io.cucumber.scala.PendingException()
+
+  Then("""{string} should be the title of the page.""") { (title: String) =>
+    val pageTitle: String = this.webDriver.getTitle
+    assert(title.equals(pageTitle))
+    this.webDriver.close()
   }
+
 }
