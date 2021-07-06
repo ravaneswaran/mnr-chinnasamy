4ec19eece77e38c82b6d8872110c223f5a95bb70
ravaneswaran <ravaneswaran@gmail.com>
2021-07-06 22:57:01 +0530

    menu css and html elements updated

--- src/main/resources/static/js/menu.js
+++ src/main/resources/static/js/menu.js
@@ -1,19 +1,23 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function toggleMenu() {
-  document.getElementById("menu").classList.toggle("show");
+    if(document.getElementById("menu").style.display == "block"){
+        document.getElementById("menu").style.display = "none";
+    } else {
+        document.getElementById("menu").style.display = "block";
+    }
 }
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
     }
-  }
 }
