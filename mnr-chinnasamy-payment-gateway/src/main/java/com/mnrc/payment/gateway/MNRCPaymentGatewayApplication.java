0da964d70d97977c6105dff9fae44b217513cc3c
ravaneswaran <ravaneswaran@gmail.com>
2021-08-04 11:06:25 +0530

    new module mnr-chinnasamy-payment-gateway-included

---
+++ mnr-chinnasamy-payment-gateway/src/main/java/com/mnrc/payment/gateway/MNRCPaymentGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.mnrc.payment.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MNRCPaymentGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MNRCPaymentGatewayApplication.class, args);
+    }
+}
