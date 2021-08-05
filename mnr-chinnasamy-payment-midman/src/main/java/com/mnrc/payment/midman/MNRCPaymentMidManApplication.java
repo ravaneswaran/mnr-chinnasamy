8008474321bd8153033e7938dfcf86e0952748e0
ravaneswaran <ravaneswaran@gmail.com>
2021-08-05 23:28:39 +0530

    individual module builds are fine now

---
+++ mnr-chinnasamy-payment-midman/src/main/java/com/mnrc/payment/midman/MNRCPaymentMidManApplication.java
@@ -0,0 +1,13 @@
+package com.mnrc.payment.midman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MNRCPaymentMidManApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MNRCPaymentMidManApplication.class, args);
+	}
+
+}
---
+++ mnr-chinnasamy-payment-midman/src/test/java/com/mnrc/payment/midman/MNRCPaymentMidManApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mnrc.payment.midman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MNRCPaymentMidManApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
