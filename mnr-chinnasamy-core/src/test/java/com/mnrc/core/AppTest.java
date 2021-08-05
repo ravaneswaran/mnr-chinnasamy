b165696db32006565e000f00639d448289cd1c14
ravaneswaran <ravaneswaran@gmail.com>
2021-08-06 00:43:04 +0530

    test classes compiled and tests are executed

--- mnr-chinnasamy-core/src/test/java/com/mnrc/core/AppTest.java
+++ mnr-chinnasamy-core/src/test/java/com/mnrc/core/AppTest.java
@@ -3,12 +3,17 @@ package com.mnrc.core;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = com.mnrc.core.App.class)
+public class AppTest {
+
     /**
      * Rigorous Test :-)
      */
