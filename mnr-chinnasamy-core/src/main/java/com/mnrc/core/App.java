24caf82128101766cb7d58d0bef04e5b605695bc
ravaneswaran <ravaneswaran@gmail.com>
2021-08-05 22:20:44 +0530

    payment-gateway module is now changed

---
+++ mnr-chinnasamy-core/src/main/java/com/mnrc/core/App.java
@@ -0,0 +1,13 @@
+package com.mnrc.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ mnr-chinnasamy-payment-gateway/src/test/java/com/mnrc/payment/gateway/AppTest.java
@@ -0,0 +1,11 @@
+package com.mnrc.payment.gateway;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+}
