adab7ac1500d31231641744f60ee819e17c8c399
ravaneswaran <ravaneswaran@gmail.com>
2021-08-06 18:24:01 +0530

    core module updated

---
+++ mnr-chinnasamy-core/src/main/java/com/mnrc/core/MNRCCoreApplication.java
@@ -0,0 +1,13 @@
+package com.mnrc.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MNRCCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MNRCCoreApplication.class, args);
+	}
+
+}
---
+++ mnr-chinnasamy-core/src/test/java/com/mnrc/core/MNRCCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mnrc.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MNRCCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
