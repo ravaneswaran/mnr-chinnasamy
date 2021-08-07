9f25d3cd54e665caac7f18aad77d7654e929ac83
ravaneswaran <ravaneswaran@gmail.com>
2021-08-07 12:36:27 +0530

    org.springframework.beans.factory.NoSuchBeanDefinitionException hunting under way

---
+++ mnr-chinnasamy-core/src/main/java/com/mnrc/core/services/impl/LoginServiceImpl.java
@@ -0,0 +1,44 @@
+package com.mnrc.core.services.impl;
+
+import com.mnrc.core.entities.User;
+import com.mnrc.core.services.LoginService;
+import com.mnrc.core.services.UserService;
+import com.mnrc.core.forms.LoginForm;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LoginServiceImpl implements LoginService {
+
+    @Autowired
+    private UserService userService;
+
+    Logger logger = LoggerFactory.getLogger(LoginServiceImpl.class);
+
+    @Override
+    public LoginForm login(String emailId, String password) {
+        User user = this.userService.getUser(emailId, password);
+        if(null != user){
+            LoginForm login = new LoginForm();
+
+            login.setFirstName(user.getFirstName());
+            login.setMiddleInitial(user.getMiddleInitial());
+            login.setLastName(user.getLastName());
+            login.setEmailId(emailId);
+            login.setPassword(password);
+            login.setUserId(user.getUUID());
+            login.setStatus(user.getStatus());
+            login.setType(user.getType());
+            login.setRoleId(user.getRole().getUUID());
+            login.setRoleName(user.getRole().getName());
+            login.setCanAccessAdministrationApp(1 == user.getRole().getCanAccessAdministrationApp());
+
+            return login;
+        } else {
+            logger.error(String.format("UNABLE TO LOGIN : the emailId and password combination does not exist"));
+            return null;
+        }
+    }
+}
---
+++ mnr-chinnasamy-core/src/main/java/com/mnrc/core/services/impl/UserProfileServiceImpl.java
@@ -0,0 +1,47 @@
+package com.mnrc.core.services.impl;
+
+import com.mnrc.core.entities.User;
+import com.mnrc.core.entities.UserProfile;
+import com.mnrc.core.repositories.UserProfileRepository;
+import com.mnrc.core.repositories.UserRepository;
+import com.mnrc.core.services.UserProfileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Component
+public class UserProfileServiceImpl implements UserProfileService {
+
+    @Autowired
+    private UserProfileRepository userProfileRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserProfile saveProfilePicture(String userId, byte[] profilePicture) {
+
+        String uuid = UUID.randomUUID().toString();
+        User user = this.userRepository.findById(userId).get();
+
+        UserProfile userProfile = this.userProfileRepository.findByUserUUID(userId);
+
+        if(null == userProfile){
+            Date now = new Date();
+            userProfile = new UserProfile();
+            userProfile.setUUID(uuid);
+            userProfile.setProfilePicture(profilePicture);
+            userProfile.setCreatedDate(now);
+            userProfile.setModifiedDate(now);
+            userProfile.setUser(user);
+        } else {
+            Date now = new Date();
+            userProfile.setProfilePicture(profilePicture);
+            userProfile.setModifiedDate(now);
+        }
+
+        return this.userProfileRepository.save(userProfile);
+    }
+}
