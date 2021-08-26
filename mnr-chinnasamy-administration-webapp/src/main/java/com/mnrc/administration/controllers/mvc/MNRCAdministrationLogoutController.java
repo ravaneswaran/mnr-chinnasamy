e86220831cfab87d8e64e94f7cf6b7e18678557b
ravaneswaran <ravaneswaran@gmail.com>
2021-08-26 12:35:49 +0530

    integration test error removed

--- mnr-chinnasamy-administration-webapp/src/main/java/com/mnrc/administration/controllers/mvc/MNRCAdministrationLogoutController.java
+++ mnr-chinnasamy-administration-webapp/src/main/java/com/mnrc/administration/controllers/mvc/MNRCAdministrationLogoutController.java
@@ -2,7 +2,6 @@ package com.mnrc.administration.controllers.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
--- mnr-chinnasamy-administration-webapp/src/test/resources/features/login/login-landing-page.feature
+++ mnr-chinnasamy-administration-webapp/src/test/resources/features/login/login-landing-page.feature
@@ -1,5 +1,5 @@
 Feature: Login Landing Page
 
   Scenario: User wants the login page
-    Given the user has typed the following address in browser address bar "http://localhost:8080/"
+    Given the user has typed the following address in browser address bar "http://localhost:8080/administration"
     Then the user should see the login page
